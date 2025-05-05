e8a1082bab32fb26bebf8b25eaadb244c176127b
Connie Leung <cleung334@gmail.com>
2025-05-05 08:12:51 +0800

    refactor: move the scss classes back to AppComponent

--- projects/in-operator-on-template-demo/src/app/app.component.ts
+++ projects/in-operator-on-template-demo/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Cat, Dog, Fish } from './animal.type';
   selector: 'app-root',
   imports: [],
   templateUrl: './app.component.html',
+  styleUrl: './app.component.scss', 
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {

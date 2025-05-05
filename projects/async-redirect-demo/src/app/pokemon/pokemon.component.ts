23fe64338d2606180050f56538eb88353c4132a4
Tushar K <tushar_kadam@idealake.com>
2025-05-05 16:40:16 +0530

    debugger 2

--- projects/async-redirect-demo/src/app/pokemon/pokemon.component.ts
+++ projects/async-redirect-demo/src/app/pokemon/pokemon.component.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, OnChanges, SimpleChanges } from '@angular/core';
 import { Pokemon } from './types/pokemon.type';
 
 @Component({
   selector: 'app-pokemon',
   imports: [],
   templateUrl: './pokemon.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonComponent {
+export default class PokemonComponent implements OnChanges {
   pokemon = input<Pokemon | undefined>(undefined);
+
+  // debugger
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pokemon']) {
+      console.log(changes);
+    }
+  }
 }

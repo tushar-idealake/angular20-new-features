a11c073e82c3df0195b28e27e6adf12b2e71f6a5
Tushar K <tushar_kadam@idealake.com>
2025-05-06 16:27:26 +0530

    status code signals httpResource

--- projects/http-resource-error-status-code/src/app/http/utils/resource-ref.util.ts
+++ projects/http-resource-error-status-code/src/app/http/utils/resource-ref.util.ts
@@ -2,13 +2,19 @@ import { HttpErrorResponse, HttpResourceRef } from "@angular/common/http";
 import { computed } from "@angular/core";
 import { Pokemon } from '../schemas/pokemon.schema';
 
-export function makeResourceRefStatus(resourceRef: HttpResourceRef<Pokemon | undefined>){
+export function makeResourceRefStatus(resourceRef: HttpResourceRef<Pokemon | undefined>){ // works like middleware
   return {
-    error: computed(() => 
+    error: computed(() => {
+      alert('API call pending');
       resourceRef.error() ? resourceRef.error() as HttpErrorResponse : undefined
+    }
     ),
     statusCode: computed(() => resourceRef.statusCode() ? resourceRef.statusCode() : undefined),
     headers: computed(() => resourceRef.headers() ? resourceRef.headers() : undefined),
-    value: computed(() => resourceRef.hasValue() ? resourceRef.value() : undefined)
+    value: computed(() => {
+      console.log(resourceRef.value(), 'changes'); // consoles and returns value if there is value
+      resourceRef.hasValue() ? resourceRef.value() : undefined
+    }
+  )
   }
 }

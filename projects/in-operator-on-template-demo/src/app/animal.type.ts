d6a47e84d9457a7ad300d6be5df956de93d20520
Tushar K <tushar_kadam@idealake.com>
2025-05-07 11:41:21 +0530

    html

--- projects/in-operator-on-template-demo/src/app/animal.type.ts
+++ projects/in-operator-on-template-demo/src/app/animal.type.ts
@@ -1,16 +1,19 @@
 export interface Animal {} 
 
 export interface Fish extends Animal {
+    name: string;
     swim: string;
 }
 
 export interface Dog extends Animal {
+    name: string;
     noise: string;
     run: string;
     swim: string
 }
 
 export interface Cat extends Animal {
+    name: string;
     noise: string;
     jump: string;
 }
--- projects/in-operator-on-template-demo/src/app/app.component.ts
+++ projects/in-operator-on-template-demo/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
 import { Cat, Dog, Fish } from './animal.type';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss', 
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,20 +19,24 @@ export class AppComponent {
 
   animals: (Cat | Dog | Fish)[] = [
     {
+      name: 'dog',
       noise: 'barks',
       run: 'a beach',
       swim: 'a swimming pool',
     },
     {
+      name: 'Cat',
       noise: 'meows',
       jump: 'on the sofa',
     },
     {
+      name: 'Fish',
       swim: 'an aquarium',
     },
     {
+      name: 'Hound',
       noise: 'growls',
-      run: 'a park',
+      run: 'a forest',
       swim: 'a pond',
     },
   ]
